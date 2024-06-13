daefee2411a6e9a8714b6184a9f7ba6cc1458413
Aobadjke5 <zyy152roy@163.com>
2024-06-13 08:25:36 +0800

    响应数据解密

--- src/utils/EncipherUtils.js
+++ src/utils/EncipherUtils.js
@@ -4,4 +4,18 @@ const MD5 = (str) => {
   return md5(str)
 }
 
-export { MD5 }
+function base64ToUtf8(base64String) {
+  // 使用atob将Base64字符串解码为二进制数据
+  const binaryString = atob(base64String);
+  // 将二进制数据转换为Uint8Array
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+  }
+  // 使用TextDecoder将Uint8Array解码为UTF-8字符串
+  const decoder = new TextDecoder("utf-8");
+  return decoder.decode(bytes);
+}
+
+export { MD5, base64ToUtf8 }
